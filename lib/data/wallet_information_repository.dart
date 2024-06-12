8d2ae66343aabd240ffed234145f9719bb981e24
Diogo Sequeira <diogocnsequeira@gmail.com>
2024-06-12 12:50:11 +0100

    add moralis endpoint

---
+++ lib/data/wallet_information_repository.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' as http;
+
+class WalletInformationRepository {
+  Future<String> getBalance(String address, String chain) async {
+    final url = Uri.http('someEndpoint', '/get_token_balance', {
+      'address': address,
+      'chain': chain,
+    });
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        return response.body;
+      }
+      return 'Bad bad';
+    } catch (e) {
+      throw Exception('Error getting Balance! $e');
+    }
+  }
+}
